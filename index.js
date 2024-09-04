76ce2ddc383040fe444ad10a36ba6878cfdee307
Maulana14 <116287549+onlybot12@users.noreply.github.com>
2024-09-05 00:37:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+
+/*
 const express = require('express');
 const app = express();
 
@@ -52,3 +55,30 @@ app.get('/galau', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+*/
+
+var express = require('express'),
+    cors = require('cors'),
+    secure = require('ssl-express-www');
+const PORT = process.env.PORT || 8080 || 5000 || 3000
+var { color } = require('./lib/color.js')
+
+var mainrouter = require('./routes/main'),
+    apirouter = require('./routes/api')
+
+var app = express()
+app.enable('trust proxy');
+app.set("json spaces",2)
+app.use(cors())
+app.use(secure)
+app.use(express.static("assets"))
+
+app.use('/', mainrouter)
+app.use('/docs', apirouter)
+
+app.listen(PORT, () => {
+    console.log(color("Server running on port " + PORT,'green'))
+})
+
+module.exports = app
