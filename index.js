dfdbfc014a5e4473ae00040834b597eafa14a82d
Maulana14 <116287549+onlybot12@users.noreply.github.com>
2024-12-08 00:51:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-
+const axios = require('axios');
 
 
 const PORT = process.env.PORT || 3000;
@@ -16,9 +16,32 @@ app.get('/', (req, res) => {
     result: "alok",
       
     })
-  
+})
 
-});
+app.get('/ai/chat', async (req, res) => {
+  let { q } = req.query;
+  if (!q) {
+    return res.status(404).json({ status: 404, creator: "MannR", msg: "Parameter 'q' tidak ditemukan" })
+  }
+  try {
+    const baseUrl = "https://hercai.onrender.com"
+    let { data } = await axios({
+      "method": "GET",
+      "url": baseUrl + "/v3/hercai",
+      "params": {
+        "question": q
+      }
+    })
+    let result = data
+    res.status(200).json({
+      status: 200,
+      creator: "MannR",
+      result
+    })
+  } catch ({ message }) {
+    res.status(500).json({ error: message });
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
